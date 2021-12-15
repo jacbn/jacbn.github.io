aab8233574fb9659ce3789aa8da97ef30f3d00a5
Jacob Brown <jeab2@cam.ac.uk>
2021-12-15 19:23:23 +0000

    datasci section

--- maths-art/tests/tests.js
+++
@@ -1,9 +0,0 @@
-import * as vec2 from '../../modules/vec2.js';
-import * as cc from '../../modules/colourConversions.js';
-import * as bc from '../../modules/basicCanvas.js'
-import Complex from '../../modules/basicComplex.js'
-
-const canvas = document.getElementById("mainCanvas");
-const ctx = canvas.getContext("2d");
-ctx.setTransform(1, 0, 0, -1, canvas.width/2, canvas.height/2);
-
