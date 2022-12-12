4c7338fb8535f3989282d8c76297a63c9cf1a82c
jacbn <jabrown405@gmail.com>
2022-12-12 15:10:36 +0000

    graphemes fixes

--- teaching/graphemes/graphemes.js
+++ teaching/graphemes/graphemes.js
@@ -25,6 +25,10 @@ WORDS TO FIX BEFORE RELEASE:
     brochure
     school
     Q, by itself
+
+TODO:
+    hello\nthere has o and t in p3?
+    deletions need fixing
 */
 
 var textWidth;
@@ -44,30 +48,45 @@ function colorInput(e) {
     const output = $('#output' + num);
     const htmlEl = document.getElementById("output" + num);
 
-    // console.log($(output.target));
-
-    // find output.target and replace the below innertext bit with the text in that
-
     const caretIndex = getCaretIndex(htmlEl);
-    
-    // if (e.data != undefined) {
-    //     console.log("LETTER: " + e.data + "; INDEX: " + caretIndex);
-    //     cardStrings[num-1] = cardStrings[num-1].substring(0, caretIndex-1) + e.data + cardStrings[num-1].substring(caretIndex-1);
-    //     console.log(cardStrings);
-    //     $('#entry1').val(cardStrings[num-1]);
-    // } else {
-    //     console.log("backspace; INDEX: " + caretIndex);
-    // }
+
+    // we use a zero-width space to allow newlines to propagate, such that when you press enter it actually shows a new line.
+    // 
+    var inputText = output.prop('innerText');
+    inputText = inputText.replace('\u200b', '');
+    if (inputText.trim() == '') {
+        output.html('');
+        setTextSize(num);
+        return;
+    }
+    while (inputText.includes('\n\n')) {
+        inputText = inputText.replace('\n\n', '\n');
+    }
+    inputText += '\u200b'
+
+    if (e.inputType == "deleteContentBackward") {
+        if (caretIndex == inputText.length) {
+            // then we're supposed to be deleting the last character, but thanks to \u200b we'll have deleted that instead
+            // so delete the now-last character in addition
+            inputText = inputText.substring(0, inputText.length-1);
+        }
+        // else, the user will have selected a character to delete, so all we need to do is set the caret to the right place
+    }
+
     
 
-    const newText = splitInput(output.prop('innerText'), num);
+    const newText = splitInput(inputText, num);
     if (typeof newText == 'str' && newText.trim() == '') return;
     output.html(newText);
 
     setCaretIndex(htmlEl, caretIndex);
 
+    setTextSize(num);
+}
 
-    var boxWidth = $('#card' + num).width();
+function setTextSize(cardNum) {
+    const output = $('#output' + cardNum);
+    var boxWidth = $('#card' + cardNum).width();
     fontSize = 48;
     output.css('font-size', '48pt');
     output.css('padding-top', '80px');
@@ -79,7 +98,9 @@ function colorInput(e) {
     }
 
     if (fontSize == 48) {
-        if (boxWidth > 0 && (textWidth > boxWidth || lineBreaks > 0)) { // if there's a line break on the card. > 0 check for uninitialised weirdness
+        // if (boxWidth > 0 && (textWidth > boxWidth || lineBreaks > 0)) {
+        if (boxWidth > 0 && (textWidth > boxWidth)) {
+            // if there's a line break on the card. > 0 check for uninitialised weirdness
             output.css('padding-top', '60px');
             output.css('font-size', '48pt');
         } else {
@@ -94,51 +115,39 @@ function splitInput(entryBox, cardIndex) {
     if (input.trim() == '') {
         return '';
     }
-
-    input = input.replace('\u200b', '');
-    while (input.includes('\n\n')) {
-        input = input.replace('\n\n', '\n');
-    }
-    input += '\u200b'
-
-    console.log("WHOLE INPUT: " + input.split('').map((e) => e.charCodeAt(0)).join(','));
+    
+    // console.log("WHOLE INPUT: " + input.split('').map((e) => e.charCodeAt(0)).join(','));
 
     splits = [];
     index = 0;
-    lineBreaks = 0;
-    var _breakExists = false;
 
+    const original = input.toLowerCase();
     var inputToProcess = input.toLowerCase();
     textWidth = getTextWidth(inputToProcess, cardIndex)
     
     while (inputToProcess.length > 0) {
-        const grapheme = takeGrapheme(inputToProcess, index);
+        const grapheme = takeGrapheme(original, inputToProcess, index);
         // console.log(grapheme.value + ": " + grapheme.value.charCodeAt(0) + "; " + '\n'.charCodeAt(0));
         if (grapheme.value == '\n') {
             splits.push($("<span>").html("<br>"));
-            _breakExists = true;
-            lineBreaks++;
-            // if (lineBreaks == 0) {
-            //     splits.push($("<span>").html("<br>"));
-            // }
         } else {
             for (char of input.substring(index, index+grapheme.length)) {
                 splits.push(constructSpan(char, phaseColors[grapheme.phase]));
             }
         }
+        // console.log("grapheme " + grapheme.value + " length: " + grapheme.length);
         inputToProcess = inputToProcess.substring(grapheme.length);
         index += grapheme.length;
     }
-    if (!_breakExists) lineBreaks = 0;
 
     return splits;
 }
 
 // compare then take the superior grapheme match (standard / regex).
 // @return value of the form [grapheme, length, phase]
-function takeGrapheme(string, index) {
+function takeGrapheme(original, string, index) {
     var standard = takeStandard(string);
-    var regexp = takeRegexp(string, index);
+    var regexp = takeRegexp(original, index);
     // prioritise by phase, or take regexp if equal
     return (regexp.phase >= standard.phase && regexp.phase != 0) ? regexp : standard;
 }
@@ -192,20 +201,25 @@ function getCanvasFont(card) {
     return `${card.css('font-weight')} 48pt ${card.css('font-family')}`;
 }
 
-//courtesy of https://javascript.plainenglish.io/how-to-find-the-caret-inside-a-contenteditable-element-955a5ad9bf81
+//modified from https://discourse.mozilla.org/t/how-to-get-the-caret-position-in-contenteditable-elements-with-respect-to-the-innertext/91068
 function getCaretIndex(element) {
     let position = 0;
     const isSupported = typeof window.getSelection !== "undefined";
     if (isSupported) {
         const selection = window.getSelection();
         if (selection.rangeCount !== 0) {
-            const range = window.getSelection().getRangeAt(0);
-            const preCaretRange = range.cloneRange();
-            preCaretRange.selectNodeContents(element);
-            preCaretRange.setEnd(range.endContainer, range.endOffset);
-            position = preCaretRange.toString().length;
+            const _range = document.getSelection().getRangeAt(0); 
+            if (!_range.collapsed) { 
+                return null; 
+            } 
+            const range = _range.cloneRange(); 
+            const temp = document.createTextNode("\0"); 
+            range.insertNode(temp); 
+            const caretposition = element.innerText.indexOf("\0"); 
+            temp.parentNode.removeChild(temp); 
+            return caretposition;          
         }
-    }
+    }    
     
     return position;
 }
@@ -213,20 +227,8 @@ function getCaretIndex(element) {
 function setCaretIndex(element, index) {
     var range = document.createRange();
     var selection = window.getSelection();
-    index
-    const children = $(element).children().toArray();
-    const breaks = Math.max(0, children.filter((el) => el.innerHTML == '<br>').length-1);
-    console.log(children);
-    
-    // range.setStart(element, index+breaks);
-    // range.collapse(true);
 
-    // console.log("text: " + $(element).text());
-
-    // l = $(element).text().length;
-    l = $(element).prop('childNodes').length;
-
-    range.setStart(element, l);
+    range.setStart(element, index);
     range.collapse(true);
     
     selection.removeAllRanges();
--- teaching/graphemes/phases.js
+++ teaching/graphemes/phases.js
@@ -1,5 +1,5 @@
 const p2 = "s a t p i m n d g o c k ck e u r h b f l".split(' ');
-const p3 = "j v w x z zz qu ch sh th ng ai ee oa oo ar or ur ow oi igh ear air ure".split(' ');
+const p3 = "j v w x z y zz qu ch sh th ng ai ee oa oo ar or ur ow oi igh ear air ure".split(' ');
 const p5 = "ay ou ie ea oy ir eu aw ey wh ph ew oe au ould oup slow cc".split(' ');
 const p3regexps_r = [/\by/g, /er\b/g];
 const p5regexps = "a.e e.e i.e o.e u[^r]e (?<=a)ti(?=on|a) (?<=gr|l)ow(?!l) (?<=nd|sh|t)ow (?<=b)ow(?=l)".split(' ');
